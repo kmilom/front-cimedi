6de623469d056f347a580f071d8e8ac71254d753
kmilom <90485091+kmilom@users.noreply.github.com>
2024-07-16 23:34:13 -0500

    login view modified

--- src/views/loginViews.js
+++ src/views/loginViews.js
@@ -3,6 +3,7 @@ import LoginForm from '../components/loginForms';
 import authService from '../services/authenticationService';
 import Footer from '../components/footer';
 import Navbar from '../components/navBar';
+import image from '../assets/imagelanding.jpg'
 
 /* aca se renderiza el componente usando un componente funcional del tipo funcion flecha que permite que no se 
 sobreescriba o exista una reasignacion*/
@@ -22,12 +23,20 @@ const LoginView = () => {
     //contenedor principal
     <div className="min-h-screen flex flex-col bg-gray-200">
       <Navbar />
-      {/* Contenedor del formulario */}
-      <div className="flex-grow flex items-center justify-center">
-        <LoginForm onSubmit={handleLogin} />
+      {/* Contenedor secundario */}
+      <div className="flex-grow flex flex-col md:flex-row items-stretch bg-yellow-50">
+        {/*<div className="w-full md:w-1/2 p-4 border border-black flex flex-col justify-center">
+          <h2>columna 1</h2>
+        </div>*/}
+        <div className="w-full md:w-1/2 border border-black">
+          <img src={image} alt="Descripción de la imagen" className="h-full w-full object-cover" />
+        </div>
+        <div className="w-full md:w-1/2 p-4 border border-black flex flex-col justify-center">
+          <LoginForm onSubmit={handleLogin} />
+        </div>
       </div>
       {/* Footer */}
-      <Footer className="bg-blue-500 text-white p-4" />
+      <Footer />
     </div>
   );
 };
