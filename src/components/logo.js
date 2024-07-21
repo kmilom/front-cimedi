b811a8f5d0731228767f24d0d8b62ca2dbb7cf57
kmilom <90485091+kmilom@users.noreply.github.com>
2024-07-21 13:13:09 -0500

    Link added to logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
     return(
         <div>
-            <Link ><img src={logo} alt="Logo de la empresa" className="w-16 h-16 object-cover rounded-full shadow-lg" /></Link>
+            <Link to="/" ><img src={logo} alt="Logo de la empresa" className="w-16 h-16 object-cover rounded-full shadow-lg" /></Link>
         </div>
     );
 }
