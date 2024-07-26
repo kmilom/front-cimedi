e8c31be10f79289e26b4f21273e8ead2e4ffa9eb
kmilom <90485091+kmilom@users.noreply.github.com>
2024-07-26 17:32:23 -0500

    doctor route added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistreView from './views/registreViews';
 import LoginView from './views/loginViews';
+import DoctorView from './views/doctorViews'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LoginView />} />
         <Route path='/registro' element={<RegistreView />} />
+        <Route path='/doctor' element={<DoctorView />} />
       </Routes>
     </Router>
   );
