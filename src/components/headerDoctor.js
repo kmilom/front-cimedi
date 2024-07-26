b1f22ab432ceaa3052ba10e5222e502c28b3569d
kmilom <90485091+kmilom@users.noreply.github.com>
2024-07-26 18:32:07 -0500

    logo user added

--- src/components/headerDoctor.js
+++ src/components/headerDoctor.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Logo from "./logo";
+import logoUser from '../assets/logouser.png'
 
 const HeaderDoctor = ({person}) => {
     return(
-        <div>
-            <div className="text-white text-2xl">
+        <div className="grid grid-cols-5">
+            <div className="text-white text-2xl col-span-2 flex items-center">
                 <h1>Bienvenido, {person.name} {person.lastName} </h1>
+            </div>
+            <div className="col-start-5 flex justify-end">
+                <img src={logoUser} alt="Logo de usuario" className="w-16 h-16 object-cover rounded-full"/>
             </div>
         </div>
     );
